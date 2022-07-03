7722c27efd09fa7b0118c5dba5b4db09b4f4e387
coldDelight <hno05039@naver.com>
2022-07-04 01:25:47 +0900

    home page layout

--- app/src/main/java/com/example/smart_campus/MainActivity.kt
+++ app/src/main/java/com/example/smart_campus/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.smart_campus
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+        val loginBtn = findViewById<Button>(R.id.button_login_login)
+        loginBtn.setOnClickListener(){
+            val intent = Intent(applicationContext, HomePage::class.java)
+            startActivity(intent)
+
+        }
     }
 }
